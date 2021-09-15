c4b9df379c9fd8c33f631fe244acb22b6fd69569
home-desktop <omermerm@gmail.com>
2021-09-15 14:09:45 -0500

    sprite and color constants + board initialization

--- tiles.py
+++ tiles.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 
+from pygame import transform
+from pygame import Surface
+from pygame import image
+
 
 class Tile(ABC):
 
@@ -20,12 +24,24 @@ class Tile(ABC):
     def get_winner(self):
         return self.won
 
+    '''
+    Returns a pygame surface visualizing the current state of the tile
+    '''
+    @abstractmethod
+    def get_visual(self):
+        pass
+
 
 #   Tic-Tac-Toe classes
 
 class TTTConst:
     DIM = 3
     OK_SYMB = {'X', 'O'}
+    COLORS = {'X':(0,0,200), 'O':(200,0,0)} # x - blue, o- red
+    GRID_SPRITE = transform.smoothscale(image.load("empty-tic-tac-toe-gray.png").convert_alpha(), (900,900))
+    P1_SPRITE = transform.smoothscale(image.load("red-splatter.png").convert_alpha(), (300,300))
+    P2_SPRITE = transform.flip(transform.smoothscale(image.load("blue-splatter.png").convert_alpha(), (300,300)), True, True)
+
 
 
 class TTTBoard(Tile):
@@ -40,11 +56,12 @@ class TTTBoard(Tile):
         [[(i, j) for i in range(TTTConst.DIM)] for j in range(TTTConst.DIM)] + \
         [[(i, i) for i in range(TTTConst.DIM)], [(i, (TTTConst.DIM-1)-i) for i in range(TTTConst.DIM)]]
 
-    def __init__(self, depth=1):
+    def __init__(self, depth=1, size):
         assert depth > 0, 'TTTBoard must have positive depth'
         super().__init__()
         self.depth = depth
-        self.initialize_board()
+        self.size = size
+        self.initialize_board(size)
 
     def __repr__(self):
         board_strs = []
@@ -64,15 +81,22 @@ class TTTBoard(Tile):
             return ans
 
     '''
-    Create a DIMxDIM array of empty boards of lower depth (tiles, if depth 1)
+    Create a DIMxDIM array of empty boards of lower depth (tiles, if depth 1). Also initialize visuals.
     '''
-    def initialize_board(self):
+    def initialize_board(self, size):
+        # create surface and draw empty grid
+        self.surface = Surface(size, size)
+        transform.smoothscale(TTTConst.GRID_SPRITE, (size,size), self.surface)
+        
+        # initialize cell locations relative to board, cell themselves, and draw result on surface
+        cell_size = size / TTTConst.DIM
+        self.cell_locs = [[(cell_size*j, cell_size*i) for j in range(TTTConst.DIM)] for i in range(TTTConst.DIM)]
         self.board = []
         for i in range(TTTConst.DIM):
             self.board.append([])
             for j in range(TTTConst.DIM):
-                self.board[i].append(TTTTile() if self.depth == 1 else TTTBoard(depth=self.depth-1))
-
+                self.board[i].append(TTTTile() if self.depth == 1 else TTTBoard(depth=self.depth-1, cell_size))
+                self.surface.blit(self.board[i][j].get_visual(), self.cell_locs[i][j])
     '''
     Receives a symbol to play and a list of pairs of length self.depth with the outermost coordinate first
     Return True if coordinate was won by play, False if win status of coordinate didn't change
